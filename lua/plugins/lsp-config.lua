e22617021b55ad47caffc1a51461fe6b8e4b3867
Mark Maalouf <shiny.bell5237@maalmark.com>
2024-02-02 00:12:18 -0600

    auto format on save

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,19 +18,18 @@ return {
 		config = function()
 			local lspconfig = require("lspconfig")
 
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
+				capabilities = capabilities,
+			})
 
 			lspconfig.gopls.setup({
-        capabilities = capabilities
-      })
-
+				capabilities = capabilities,
+			})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "gD", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "gD", vim.lsp.buf.references, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,9 +6,23 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.gofumpt,
-        null_ls.builtins.diagnostics.golangci_lint,
+				null_ls.builtins.formatting.gofumpt,
+				null_ls.builtins.diagnostics.golangci_lint,
 			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+							-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+                            vim.lsp.buf.format({ async = false })
+						end,
+					})
+				end
+			end,
 		})
 
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
