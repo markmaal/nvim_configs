9510f5ea15e0fc54576a506a8eba80b1bf0b6023
Mark Maalouf <shiny.bell5237@maalmark.com>
2024-02-01 23:12:50 -0600

    add auto-session and fix neo-tree for sessions

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,6 +5,11 @@ return {
 			options = {
 				theme = "catppuccin",
 			},
+            sections = {
+                lualine_c = {
+                    require('auto-session.lib').current_session_name
+                }
+            }
 		})
 	end
 }
